c4d8937bafec8ee417e954b11a4138dd2b9c064f
JoelDeAriovaldo <joeldeariovaldodevef@gmail.com>
2024-04-12 13:37:46 +0200

    Front completo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todoList = [
+    {
+      itemName: 'Coding',
+      itemDueDate: '04/12/2024',
+      itemPriority: 'high',
+      itemCategory: 'work',
+    },
+    {
+      itemName: 'Design',
+      itemDueDate: '04/13/2024',
+      itemPriority: 'low',
+      itemCategory: 'work',
+    },
+    {
+      itemName: 'Shopping',
+      itemDueDate: '04/15/2024',
+      itemPriority: 'middle',
+      itemCategory: 'Personal',
+    },
+    {
+      itemName: 'Workout',
+      itemDueDate: '04/12/2024',
+      itemPriority: 'high',
+      itemCategory: 'Personal',
+    },
+  ];
+
+  today: number = Date.now();
 
   constructor() {}
-
 }
